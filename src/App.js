a5b9732cdf3a13aa6eb06f7c70df685d6340d93b
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-18 22:41:55 +0530

    changed to className

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div className="container text-center mt-5">
-        <h1>Gallery</h1>
-        <div className="row">
-          {Array.apply(null, { length: 9 }).map(() => {
-            return (
-              <div className="col mb-5">
-                <div class="card" style={{ width: "18rem" }}>
-                  <img
-                    src="https://via.placeholder.com/200"
-                    class="card-img-top"
-                    alt="idk"
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <Navbar />
     </>
   );
 }
