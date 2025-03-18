1b30fdd4aa96ed3ce6af1ae9c08830ef22390dbf
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-18 23:34:13 +0530

    use of hooks to add and collapse images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 const photos = [
   "https://picsum.photos/id/1001/200/200",
@@ -12,13 +13,28 @@ const photos = [
 ];
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapsed] = useState(false);
+  const toggle = () => collapsed(!isCollapsed);
+  
   return (
     <>
       <Navbar />
       <div className="container text-center mt-5">
+        <button
+          className="btn btn-warning mx-2"
+          onClick={() =>
+            setItems(["https://picsum.photos/id/1009/200/200", ...items])
+          }
+        >
+          +Add
+        </button>
+        <button className="btn btn-success" onClick={toggle}>
+          Collapse
+        </button>
         <h1>Gallery</h1>
         <div className="row">
-          {photos.map((photo) => (
+          {items.map((photo) => (
             <Card src={photo} />
           ))}
         </div>
