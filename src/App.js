ecb2eade271a4b44195cc1c6617e11c9bd7791fc
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-19 23:30:44 +0530

    adding context

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
 import "./App.css";
 import Card from "./components/Card";
 import Layout from "./components/Layout";
-import { useReducer, useMemo } from "react";
-
-const photos = [];
-
-const initialState = {
-  items: photos,
-  count: photos.length,
-  inputs: { title: null, file: null, path: null },
-  isCollapsed: false,
-};
-
-const handleOnChange = (state, e) => {
-  if (e.target.name === "file") {
-    return {
-      ...state.inputs,
-      file: e.target.files[0],
-      path: URL.createObjectURL(e.target.files[0]),
-    };
-  } else {
-    return { ...state.inputs, title: e.target.value };
-  }
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "setItem":
-      return {
-        ...state,
-        items: [state.inputs, ...state.items],
-        count: state.items.length + 1,
-        inputs: { title: null, file: null, path: null },
-      };
-    case "setInputs":
-      return {
-        ...state,
-        inputs: handleOnChange(state, action.payload.value),
-      };
-    case "collapsed":
-      return {
-        ...state,
-        isCollapsed: action.payload.bool,
-      };
-    default:
-      return state;
-  }
-}
+import { Context } from "./context";
+import { useMemo, useContext } from "react";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { dispatch, state } = useContext(Context);
   const toggle = (bool) => dispatch({ type: "collapsed", payload: { bool } });
   const handleOnChange = (e) =>
     dispatch({ type: "setInputs", payload: { value: e } });
