d06b22c668cc618f6827d6072341f3cf88d30663
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-19 11:38:17 +0530

    added custom image upload option and also added counter to count total images

--- src/App.js
+++ src/App.js
@@ -2,34 +2,53 @@ import "./App.css";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import UploadForm from "./components/UploadForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const photos = [
-  "https://picsum.photos/id/1001/200/200",
-  "https://picsum.photos/id/1002/200/200",
-  "https://picsum.photos/id/1003/200/200",
-  "https://picsum.photos/id/1004/200/200",
-  "https://picsum.photos/id/1005/200/200",
-  "https://picsum.photos/id/1006/200/200",
-];
+const photos = ["https://picsum.photos/id/1001/200/200"];
 
 function App() {
+  const [count, setCount] = useState();
+  const [input, setInput] = useState({ title: null, file: null, path: null });
   const [items, setItems] = useState(photos);
   const [isCollapsed, collapsed] = useState(false);
   const toggle = () => collapsed(!isCollapsed);
+  const handleOnChange = (e) => {
+    if (e.target.name === "file") {
+      setInput({
+        ...input,
+        file: e.target.files[0],
+        path: URL.createObjectURL(e.target.files[0]),
+      });
+    } else {
+      setInput({ ...input, title: e.target.value });
+    }
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setItems([input.path, ...items]);
+  };
+  useEffect(() => {
+    setCount(`You have ${items.length} image${items.length > 1 ? "s" : ""}`);
+  }, [items]);
 
   return (
     <>
       <Navbar />
       <div className="container text-center mt-5">
         <button className="btn btn-success float-end" onClick={toggle}>
-          +Add
+          {isCollapsed ? "Close" : "+Add"}
         </button>
-        <UploadForm isVisible={isCollapsed} />
+        <div className="clearfix mb-4"></div>
+        <UploadForm
+          isVisible={isCollapsed}
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        />
+        {count}
         <h1>Gallery</h1>
         <div className="row">
-          {items.map((photo) => (
-            <Card src={photo} />
+          {items.map((photo, index) => (
+            <Card key={index} src={photo} />
           ))}
         </div>
       </div>
