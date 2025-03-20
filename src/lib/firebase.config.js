4bb6126af457f2a846c988548e43c0be19d78686
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-20 19:46:49 +0530

    setting up environment variables

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCN4gpaAXlZxCZuaNscmoMYboh5xNr45f0",
-  authDomain: "snapshot-bd0a5.firebaseapp.com",
-  projectId: "snapshot-bd0a5",
-  storageBucket: "snapshot-bd0a5.firebasestorage.app",
-  messagingSenderId: "204455033669",
-  appId: "1:204455033669:web:4a57be83fa2addbdc79c2c",
-  measurementId: "G-6492RD4NNW",
+
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
