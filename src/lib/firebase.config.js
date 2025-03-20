34bdbb1065cee4683b59deccc84b90c947f382b9
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-20 10:25:04 +0530

    added firebase

---
+++ src/lib/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCN4gpaAXlZxCZuaNscmoMYboh5xNr45f0",
+  authDomain: "snapshot-bd0a5.firebaseapp.com",
+  projectId: "snapshot-bd0a5",
+  storageBucket: "snapshot-bd0a5.firebasestorage.app",
+  messagingSenderId: "204455033669",
+  appId: "1:204455033669:web:4a57be83fa2addbdc79c2c",
+  measurementId: "G-6492RD4NNW",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
