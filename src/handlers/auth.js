f92bb2134ef3f4e6dc81ae9186b700c1d4c40f8b
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-03-22 17:44:08 +0530

    adding auth

---
+++ src/handlers/auth.js
@@ -0,0 +1,21 @@
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "../lib/firebase.config";
+
+const provider = new GoogleAuthProvider();
+
+const FirebaseAuth = {
+  signIn: () => {
+    return new Promise((resolve) => {
+      signInWithPopup(auth, provider)
+        .then((response) => {
+          resolve(response.user);
+        })
+        .catch(console.error);
+    });
+  },
+  signOut: () => {
+    signOut(auth).then(console.log("User logged out")).catch(console.error);
+  },
+};
+
+export default FirebaseAuth;
